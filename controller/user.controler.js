77c53446d7654e8d6edea4a34667df528072329f
Abdias1998 <lagraceparle98@gmail.com>
2023-05-05 08:32:36 +0100

    envoie1

--- controller/user.controler.js
+++ controller/user.controler.js
@@ -551,9 +551,7 @@ module.exports.upload_profil = async_handler(async (req, res) => {
         setDefaultsOnInsert: true,
       }
     );
-    return res
-      .status(200)
-      .json({ message: "Profil mise à jour, raffraîchissez la page ! " });
+    return res.status(200).json({ message: "Profil mise à jour !" });
   } catch (error) {
     return res.status(500).json({
       message: `Erreur interne du serveur, veuillez réessayez plus tard ${error} !`,
