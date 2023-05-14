e043f389cd0166d158f997f06cd6c5140806cce8
Abdias1998 <lagraceparle98@gmail.com>
2023-05-14 15:14:56 +0100

    Retour à zéro

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,27 +5,13 @@ const partition_controller = require("../controller/partition.controller");
 const middleware = require("../middlewre/verify.token");
 
 const multer = require("multer");
-const generateCode = () => {
-  let code = "";
-  for (let i = 0; i < 4; i++) {
-    code += Math.floor(Math.random() * 10);
-  }
-  return code;
-};
-const date = new Date();
-const formattedDate = `${date.getFullYear()}${(date.getMonth() + 1)
-  .toString()
-  .padStart(2, "0")}${date.getDate().toString().padStart(2, "0")}`;
 
 const storages = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./partition");
   },
-  // filename: (req, file, cb) => {
-  //   cb(null, `${file.originalname}`);
-  // },
   filename: (req, file, cb) => {
-    cb(null, `IMG-${formattedDate}-LGP-${generateCode()}.jpg`);
+    cb(null, `${file.originalname}`);
   },
 });
 const uploads = multer({ storage: storages });
--- routes/video.routes.js
+++ routes/video.routes.js
@@ -4,29 +4,16 @@ const video_controler = require("../controller/video.controler");
 // const middleware = require("../middlewre/verify.token");
 const multer = require("multer");
 
-const generateCode = () => {
-  let code = "";
-  for (let i = 0; i < 4; i++) {
-    code += Math.floor(Math.random() * 10);
-  }
-  return code;
-};
-const date = new Date();
-const formattedDate = `${date.getFullYear()}${(date.getMonth() + 1)
-  .toString()
-  .padStart(2, "0")}${date.getDate().toString().padStart(2, "0")}`;
-
 const storages = multer.diskStorage({
   destination: (req, file, cb) => {
     // cb(null, "./client/build/audio");
     cb(null, "./video");
   },
   filename: (req, file, cb) => {
-    cb(null, `Video-${formattedDate}-LGP-${generateCode()}`);
+    cb(null, `${file.originalname}`);
   },
 });
 const uploads = multer({ storage: storages });
-router.put("/views/:id", video_controler.ViewsMiddelware);
 router.get("/read", video_controler.readVideo); /**Lire une partition */
 router.post(
   "/create",
@@ -34,6 +21,7 @@ router.post(
   uploads.single("video"),
   video_controler.createVideo
 ); /**Créer une partition */
+router.put("/views/:id", video_controler.ViewsMiddelware);
 router.delete(
   "/delete_video/:id",
   video_controler.deleteVideo
